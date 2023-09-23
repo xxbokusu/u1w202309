efadd93e3d2d0019368ecb1962ee1fdbaff8a356
Noa <boxship.noah@gmail.com>
2023-09-23 15:42:16 +0900

    mod sound load async

--- u1w202309_one_button/Assets/Scripts/Manager/SoundManager.cs
+++ u1w202309_one_button/Assets/Scripts/Manager/SoundManager.cs
@@ -1,21 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using ScriptableObject;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-namespace unity1week202309.Manager
-{
+namespace unity1week202309.Manager {
     /*
      * <summery>
      * BGM/SEの再生と停止を管理する
      * 同時に再生するものの数だけAudioSourceを用意する
      * </summery>
      */
-    public class SoundManager: MonoBehaviour {
-        private static SoundManager _instance;
-        public static SoundManager Instance { get { return _instance; } }
+    public class SoundManager : MonoBehaviour {
+        public static SoundManager Instance { get; private set; }
 
         private enum SoundState {
             None,
@@ -24,62 +24,78 @@ namespace unity1week202309.Manager
             Playing
         }
         private SoundState _currentState = SoundState.None;
-        public bool IsReady { get { return _currentState == SoundState.Ready; } }
-        public bool IsPlaying { get { return _currentState == SoundState.Playing; } }
+
+        public bool IsReady => _currentState == SoundState.Ready;
+        public bool IsPlaying => _currentState == SoundState.Playing;
+
+        private Dictionary<string, AudioClip> _bgmDictionary = new Dictionary<string, AudioClip>() {
+            { "Sparrow-Real_Ambi01-1", null },
+        };
+
+        private Dictionary<string, AudioClip> _seDictionary = new Dictionary<string, AudioClip>() {
+            { "maou_se_voice_bird01", null },
+            { "Crow-Real_Ambi01-1", null}
+        };
+
+        private Dictionary<string, AudioSource> _audioSourceDictionary = new Dictionary<String, AudioSource>();
+
+        private const float BGMVolumeMax = 0.5f;
+
+        // ScriptableObjectから音量の倍率を取得する
+        [SerializeField] private ConfigScriptableObject _configScriptableObject = null;
         
-        private Dictionary<String, AudioClip> _bgmDictionary = new Dictionary<String, AudioClip>() {
-            {"maou_bgm_healing11b(moving)", null},
-            {"Sparrow-Real_Ambi01-1", null}
-        };
-        private Dictionary<String, AudioClip> _seDictionary = new Dictionary<String, AudioClip>() {
-            {"maou_se_sound_pc01(click)", null},
-            {"maou_se_battle07(shoot)", null},
-            {"maou_se_voice_bird01",null}
-        };
-
-        private Dictionary<String, AudioSource> _audioSourceDictionary = new Dictionary<String, AudioSource>();
-        private const float BGMVolume = 0.5f;
-
         void Start() {
-            if (_instance == null) {
-                _instance = this;
+            if (Instance == null) {
+                Instance = this;
                 DontDestroyOnLoad(gameObject);
-            } else {
+            }
+            else {
                 Destroy(gameObject);
             }
+            if (_configScriptableObject == null) {
+                Debug.Util.LogError("SoundManager::Start()::configScriptableObject is null");
+                return;
+            }
+
             _currentState = SoundState.Loading;
-            LoadAudioClipsAsync().Forget();
+            LoadAudioClipsAsync(this.GetCancellationTokenOnDestroy()).Forget();
         }
 
-        private async UniTaskVoid LoadAudioClipsAsync() {
-            var bgmKeys = new List<string>(_bgmDictionary.Keys);
-            foreach (var key in bgmKeys)
-            {
-                string filePath = "Audios/BGM/" + key;
-                AudioClip clip = Resources.Load<AudioClip>(filePath);
-                Assert.IsNotNull(clip, "SoundManager::loadSoundAsync()::clip is null! key = " + filePath);
+        private async UniTaskVoid LoadAudioClipsAsync(CancellationToken token) {
+            var bgmRequestDictionary = new Dictionary<string, ResourceRequest>();
+            foreach (var key in _bgmDictionary.Keys) {
+                var req = Resources.LoadAsync<AudioClip>("Audios/BGM/" + key);
+                bgmRequestDictionary.Add(key, req);
+            }
+            var seRequestDictionary = new Dictionary<string, ResourceRequest>();
+            foreach (var key in _seDictionary.Keys) {
+                var req = Resources.LoadAsync<AudioClip>("Audios/SE/" + key);
+                seRequestDictionary.Add(key, req);
+            }
+
+            await UniTask.WaitUntil(() => bgmRequestDictionary.Values.All(req => req.isDone), cancellationToken: token);
+            foreach (var key in bgmRequestDictionary.Keys) {
+                var clip = bgmRequestDictionary[key].asset as AudioClip;
+                Assert.IsNotNull(clip, "SoundManager::loadSoundAsync()::clip is null! key = " + key);
                 _bgmDictionary[key] = clip;
 
                 // 再生用のAudioSourceを子オブジェクトとして生成
-                GameObject child = new GameObject(key);
+                var child = new GameObject(key);
                 child.transform.SetParent(transform);
                 var audioSource = child.AddComponent<AudioSource>();
                 // ループ再生設定
                 audioSource.loop = true;
                 _audioSourceDictionary.Add(key, audioSource);
             }
-            // SEをロード
-            var seKeys = new List<string>(_seDictionary.Keys);
-            foreach (var key in seKeys)
-            {
-                // unityroomでのAddressablesのロードがうまくいかないので、Resourcesからロードする
-                string filePath = "Audios/SE/" + key;
-                AudioClip clip = Resources.Load<AudioClip>(filePath);
-                Assert.IsNotNull(clip, "SoundManager::loadSoundAsync()::clip is null! key = " + filePath);
+            
+            await UniTask.WaitUntil(() => seRequestDictionary.Values.All(req => req.isDone), cancellationToken: token);
+            foreach (var key in seRequestDictionary.Keys) {
+                var clip = seRequestDictionary[key].asset as AudioClip;
+                Assert.IsNotNull(clip, "SoundManager::loadSoundAsync()::clip is null! key = " + key);
                 _seDictionary[key] = clip;
 
                 // 再生用のAudioSourceを子オブジェクトとして生成
-                GameObject child = new GameObject(key);
+                var child = new GameObject(key);
                 child.transform.SetParent(transform);
                 var audioSource = child.AddComponent<AudioSource>();
                 _audioSourceDictionary.Add(key, audioSource);
@@ -87,87 +103,98 @@ namespace unity1week202309.Manager
 
             _currentState = SoundState.Ready;
         }
-        
+
         // BGMを再生する
-        public void PlayBGM(string bgmName)
-        {
-            if (!_bgmDictionary.ContainsKey(bgmName))
-            {
+        public void PlayBGM(string bgmName) {
+            if (!_bgmDictionary.ContainsKey(bgmName)) {
                 Debug.Util.Log("SoundManager::PlayBGM()::bgmName is not found!");
                 return;
             }
 
             // 再生用のAudioSourceを取得して再生する
             AudioSource audioSource = _audioSourceDictionary[bgmName];
-            if (audioSource == null)
-            {
+            if (audioSource == null) {
                 Debug.Util.Log("SoundManager::PlayBGM()::AudioSource is not found!");
                 return;
             }
+
             audioSource.clip = _bgmDictionary[bgmName];
             FadeInAsync(audioSource, this.GetCancellationTokenOnDestroy()).Forget();
             audioSource.Play();
             _currentState = SoundState.Playing;
         }
 
-        // 音量を徐々に大きくすることでフェードインする
-        private async UniTaskVoid FadeInAsync(AudioSource audioSource, CancellationToken token)
-        {
+        private bool CanSetVolume(AudioSource audioSource, float volume) {
+            if (volume < 0.0f) return false;
+            if (volume > BGMVolumeMax * _configScriptableObject.GetBGMVolumeRate()) {
+                audioSource.volume = BGMVolumeMax * _configScriptableObject.GetBGMVolumeRate();
+                return false;
+            }
+            return true;
+        }
+        
+        // 再生中のaudioSourceの音量を変更する
+        public void ResetBGMVolume() {
+            if (_currentState != SoundState.Playing) {
+                return;
+            }
 
+            Debug.Util.LogFormat("SoundManager::ResetBGMVolume()::Reset BGM Volume to {0}", _configScriptableObject.GetBGMVolumeRate());
+            foreach (var audioSource in _audioSourceDictionary.Values) {
+                if (!audioSource.isPlaying) continue;
+                audioSource.volume = audioSource.volume * _configScriptableObject.GetBGMVolumeRate() * 2.0f;
+            }
+        }
+        
+        // 音量を徐々に大きくすることでフェードインする
+        private async UniTaskVoid FadeInAsync(AudioSource audioSource, CancellationToken token) {
             audioSource.volume = 0.0f;
-            while (audioSource.volume < BGMVolume)
-            {
-                audioSource.volume += 0.01f;
+            while (CanSetVolume(audioSource, audioSource.volume + 0.01f)) {
+                audioSource.volume = (audioSource.volume + 0.01f) * _configScriptableObject.GetBGMVolumeRate();
                 await UniTask.Delay(10);
             }
         }
 
         // BGMを停止する
-        public void StopBGM(string bgmName)
-        {
-            if (_currentState != SoundState.Playing)
-            {
+        public void StopBGM(string bgmName) {
+            if (_currentState != SoundState.Playing) {
                 return;
             }
 
             // 音声をフェードアウトさせる
             AudioSource audioSource = _audioSourceDictionary[bgmName];
-            if (audioSource == null)
-            {
+            if (audioSource == null) {
                 Debug.Util.Log("SoundManager::StopBGM()::AudioSource is not found!");
                 return;
             }
+
             FadeOutAsync(audioSource, this.GetCancellationTokenOnDestroy()).Forget();
             audioSource.Stop();
             _currentState = SoundState.Ready;
         }
 
         //フェードアウト
-        private async UniTaskVoid FadeOutAsync(AudioSource audioSource, CancellationToken token)
-        {
-            while (audioSource.volume > 0.0f)
-            {
-                audioSource.volume -= 0.01f;
+        private async UniTaskVoid FadeOutAsync(AudioSource audioSource, CancellationToken token) {
+            while (CanSetVolume(audioSource, audioSource.volume - 0.01f)) {
+                audioSource.volume = (audioSource.volume - 0.01f) * _configScriptableObject.GetBGMVolumeRate();
                 await UniTask.Delay(10);
             }
         }
 
         // SEを再生する
-        public void PlaySE(string seName)
-        {
-            if (!_seDictionary.ContainsKey(seName))
-            {
+        public void PlaySE(string seName) {
+            if (!_seDictionary.ContainsKey(seName)) {
                 Debug.Util.Log("SoundManager::PlaySE()::seName is not found!");
                 return;
             }
 
             // 再生用のAudioSourceを取得して再生する
             AudioSource audioSource = _audioSourceDictionary[seName];
-            if (audioSource == null)
-            {
+            if (audioSource == null) {
                 Debug.Util.Log("SoundManager::PlaySE()::AudioSource is not found!");
                 return;
             }
+
             audioSource.volume = 0.5f;
             audioSource.PlayOneShot(_seDictionary[seName]);
         }
