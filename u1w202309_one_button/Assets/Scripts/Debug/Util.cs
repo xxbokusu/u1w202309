86c01dd05bc1d9e8962cfe8a816e294e265fab24
Noa <boxship.noah@gmail.com>
2023-09-25 19:43:45 +0900

    hide on prod

--- u1w202309_one_button/Assets/Scripts/Debug/Util.cs
+++ u1w202309_one_button/Assets/Scripts/Debug/Util.cs
@@ -11,35 +11,35 @@ namespace unity1week202309.Debug
         //        private static extern void ConsoleLog(string logString);
 
 
-        //    [Conditional("EDITOR_DEBUG")]
+        [Conditional("EDITOR_DEBUG")]
         public static void Log(string _message)
         {
             UnityEngine.Debug.Log(_message);
         }
 
 
-        //      [Conditional("EDITOR_DEBUG")]
+        [Conditional("EDITOR_DEBUG")]
         public static void LogFormat(string _message, params object[] _params)
         {
             UnityEngine.Debug.LogFormat(_message, _params);
         }
 
 
-        //        [Conditional("EDITOR_DEBUG")]
+        [Conditional("EDITOR_DEBUG")]
         public static void LogWarning(string _message)
         {
             UnityEngine.Debug.LogWarning(_message);
         }
 
 
-        //      [Conditional("EDITOE_DEBUG")]
+        [Conditional("EDITOE_DEBUG")]
         public static void LogWarningFormat(string _message, params object[] _params)
         {
             UnityEngine.Debug.LogWarningFormat(_message, _params);
         }
 
 
-        //        [Conditional("EDITOR_DEBUG")]
+        [Conditional("EDITOR_DEBUG")]
         public static void LogError(string _message)
         {
             UnityEngine.Debug.LogError(_message);
