b2614b203630a8775f1dceca111da81faaf4a442
Noa <boxship.noah@gmail.com>
2023-09-25 20:39:53 +0900

    初手のフィードバック強化

--- u1w202309_one_button/Assets/Scripts/Controller/GirlController.cs
+++ u1w202309_one_button/Assets/Scripts/Controller/GirlController.cs
@@ -10,22 +10,27 @@ namespace unity1week202309.Controller {
     public class GirlController : MonoBehaviour {
         private enum GirlState {
             Waiting,
+            WarmUp,
             Walking,
             Running,
             Exhausted,
             Resulting
         }
+
         private GirlState _state = GirlState.Waiting;
+
         private readonly Dictionary<GirlState, float> _wasteResourceByState = new() {
-            { GirlState.Waiting , 2.0f},
-            { GirlState.Walking , 3.0f},
-            { GirlState.Running , 4.0f},
-            { GirlState.Exhausted , 4.0f},
-            { GirlState.Resulting , 0.0f},
+            { GirlState.Waiting, 1.0f },
+            { GirlState.WarmUp, 2.0f },
+            { GirlState.Walking, 3.0f },
+            { GirlState.Running, 4.0f },
+            { GirlState.Exhausted, 4.0f },
+            { GirlState.Resulting, 0.0f },
         };
 
         private readonly Dictionary<GirlState, string> _stateNameByState = new() {
-            { GirlState.Waiting, "WAIT02" },
+            { GirlState.Waiting, "WAIT00" },
+            { GirlState.WarmUp, "WAIT02" },
             { GirlState.Walking, "WALK00_F" },
             { GirlState.Running, "RUN00_F" },
             { GirlState.Exhausted, "REFLESH00" },
@@ -33,15 +38,18 @@ namespace unity1week202309.Controller {
 
         private Transform _charaTransform;
         private Animator _animator;
-        public bool IsWaitingState => _animator.GetCurrentAnimatorStateInfo(0).IsName(_stateNameByState[GirlState.Waiting]);
-        public bool ParamIsWalking { get; private set; } = false;
+
+        public bool IsWaitingState =>
+            _animator.GetCurrentAnimatorStateInfo(0).IsName(_stateNameByState[GirlState.Waiting]);
+
+        public bool IsWarmUpState =>
+            _animator.GetCurrentAnimatorStateInfo(0).IsName(_stateNameByState[GirlState.WarmUp]);
 
         public bool IsWalkingState =>
             _animator.GetCurrentAnimatorStateInfo(0).IsName(_stateNameByState[GirlState.Walking]);
-
-        public bool ParamIsRunning { get; private set; } = false;
-
+        
         private bool ParamIsExhausted { get; set; } = false;
+
         public bool IsRunningState =>
             _animator.GetCurrentAnimatorStateInfo(0).IsName(_stateNameByState[GirlState.Running]);
 
@@ -49,7 +57,7 @@ namespace unity1week202309.Controller {
 
         private MainSceneManager _mainSceneManager;
         private PowerChargerController _powerChargerController;
-        
+
         void Start() {
             _animator = GetComponent<Animator>();
             _charaTransform = GetComponent<Transform>();
@@ -74,6 +82,7 @@ namespace unity1week202309.Controller {
                 Debug.Util.LogError("GirlController::Start()::_powerChargerController is null");
                 return;
             }
+
             _powerChargerController.Activate();
         }
 
@@ -82,50 +91,54 @@ namespace unity1week202309.Controller {
             if (_mainSceneManager.IsResult) {
                 _state = GirlState.Resulting;
                 _powerChargerController.Deactivate();
-                ParamIsWalking = false;
-                ParamIsRunning = false;
-                _animator.SetBool("IsWalking", ParamIsWalking);
-                _animator.SetBool("IsRunning", ParamIsRunning);
+                _animator.SetBool("IsWalking", false);
+                _animator.SetBool("IsRunning", false);
+                _animator.SetBool("IsWarmUp", false);
                 return;
             }
 
             if (_powerChargerController.Power > 15.0f) {
                 _state = GirlState.Exhausted;
                 ParamIsExhausted = true;
-                _animator.SetBool("IsExthausted", ParamIsExhausted);
+                _animator.SetBool("IsExthausted", true);
             }
+
             if (ParamIsExhausted) {
-                if (_powerChargerController.Power < 2.0f) {
+                if (_powerChargerController.Power < 1.0f) {
                     ParamIsExhausted = false;
-                    _animator.SetBool("IsExthausted", ParamIsExhausted);
+                    _animator.SetBool("IsExthausted", false);
                     _state = GirlState.Waiting;
-                } else {
+                }
+                else {
                     return;
                 }
             }
 
             switch (_powerChargerController.Power) {
                 case > 10.0f:
-                    ParamIsWalking = true;
-                    ParamIsRunning = true;
                     _state = GirlState.Running;
-                    _animator.SetBool("IsRunning", ParamIsRunning);
+                    _animator.SetBool("IsRunning", true);
                     break;
                 case > 2.0f:
-                    ParamIsWalking = true;
-                    ParamIsRunning = false;
                     _state = GirlState.Walking;
-                    _animator.SetBool("IsWalking", ParamIsWalking);
+                    _animator.SetBool("IsWalking", true);
+                    break;
+                case > 1.0f:
+                    _state = GirlState.WarmUp;
+                    _animator.SetBool("IsWalking", false);
+                    _animator.SetBool("IsRunning", false);
+                    _animator.SetBool("IsWarmUp", true);
                     break;
                 default:
-                    ParamIsWalking = false;
-                    ParamIsRunning = false;
                     _state = GirlState.Waiting;
-                    _animator.SetBool("IsWalking", ParamIsWalking);
-                    _animator.SetBool("IsRunning", ParamIsRunning);
+                    _animator.SetBool("IsWalking", false);
+                    _animator.SetBool("IsRunning", false);
+                    _animator.SetBool("IsWarmUp", false);
                     break;
             }
+
             if (IsWaitingState) return;
+            if (IsWarmUpState) return;
             if (IsResulting) return;
 
             var moveVector = new Vector3(0.5f, 0, 0);
@@ -136,7 +149,7 @@ namespace unity1week202309.Controller {
             _mainSceneManager.AddScore(moveVector.magnitude);
             await UniTask.Delay(1000);
         }
-        
+
         // stateに応じてキャラのFace rigを切り替える
         public void ChangeFaceRig() {
             switch (_state) {
